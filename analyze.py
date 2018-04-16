81220c0afd89c184293a5e044249a11b6d323a9c
Alex Brizius <abrizius@nd.edu>
2018-04-16 09:17:43 -0400

    Updated analyze.py script to output results more neatly

--- analyze.py
+++ analyze.py
@@ -31,6 +31,9 @@ if __name__ == '__main__':
         usage(sys.argv[0])
         sys.exit(1)
 
+    # print column labels first 
+    print "Accuracy Precision Recall F1-Score"
+
     table_ordinal = pd.read_csv('./data/' + sys.argv[1] + '_overlap_ordinal.csv')
     table_numeric = pd.read_csv('./data/' + sys.argv[1] + '_overlap_numeric.csv')
     
@@ -49,10 +52,11 @@ if __name__ == '__main__':
     export_graphviz(dt_entropy, out_file=dotfile, feature_names=features)
     (tree, ) = pydot.graph_from_dot_file(dotfile)
     tree.write_png(png)
-
+    
+    
     # test
     y_predict = dt_entropy.predict(X_test)
-    print "Entropy: " + str(accuracy_score(y_test, y_predict)) + ", " + str(precision_score(y_test, y_predict)) + ", " + str(recall_score(y_test, y_predict)) + ", " + str(f1_score(y_test, y_predict))
+    print "Entropy " + str(accuracy_score(y_test, y_predict))[:5] + ' ' + str(precision_score(y_test, y_predict))[:5] + ' ' + str(recall_score(y_test, y_predict))[:5] + ' ' + str(f1_score(y_test, y_predict))[:5]
 
     X_train, X_test, y_train, y_test = train_test_split(table_numeric[features], table_numeric[target], random_state=1)
     dt_gini = DecisionTreeClassifier(criterion='gini', max_depth=3)    
@@ -67,23 +71,23 @@ if __name__ == '__main__':
     tree.write_png(png)
 
     y_predict = dt_gini.predict(X_test)
-    print "Gini: " + str(accuracy_score(y_test, y_predict)) + ", " + str(precision_score(y_test, y_predict)) + ", " + str(recall_score(y_test, y_predict)) + ", " + str(f1_score(y_test, y_predict))
+    print "Gini " + str(accuracy_score(y_test, y_predict))[:5] + ' ' + str(precision_score(y_test, y_predict))[:5] + ' ' + str(recall_score(y_test, y_predict))[:5] + ' ' + str(f1_score(y_test, y_predict))[:5]
      
     gauss = GaussianNB()
     y_predict = gauss.fit(X_train, y_train).predict(X_test)
-    print "Bayes: " + str(accuracy_score(y_test, y_predict)) + ", " + str(precision_score(y_test, y_predict)) + ", " + str(recall_score(y_test, y_predict)) + ", " + str(f1_score(y_test, y_predict))
+    print "Bayes " + str(accuracy_score(y_test, y_predict))[:5] + ' ' + str(precision_score(y_test, y_predict))[:5] + ' ' + str(recall_score(y_test, y_predict))[:5] + ' ' + str(f1_score(y_test, y_predict))[:5]
 
     random_forest = RandomForestClassifier(criterion='entropy', max_depth=3)
     X_train, X_test, y_train, y_test = train_test_split(table_ordinal[features], table_ordinal[target], random_state=1)
     random_forest.fit(X_train, y_train)
     y_predict = random_forest.predict(X_test)
-    print "Random Forest: " + str(accuracy_score(y_test, y_predict)) + ", " + str(precision_score(y_test, y_predict)) + ", " + str(recall_score(y_test, y_predict)) + ", " + str(f1_score(y_test, y_predict))
+    print "RandomForest " + str(accuracy_score(y_test, y_predict))[:5] + ' ' + str(precision_score(y_test, y_predict))[:5] + ' ' + str(recall_score(y_test, y_predict))[:5] + ' ' + str(f1_score(y_test, y_predict))[:5]
     
     ada = AdaBoostClassifier(random_state=1)
     X_train, X_test, y_train, y_test = train_test_split(table_ordinal[features], table_ordinal[target], random_state=1)
     ada.fit(X_train, y_train)
     y_predict = ada.predict(X_test)
-    print "AdaBoost: " + str(accuracy_score(y_test, y_predict)) + ", " + str(precision_score(y_test, y_predict)) + ", " + str(recall_score(y_test, y_predict)) + ", " + str(f1_score(y_test, y_predict))
+    print "AdaBoost " + str(accuracy_score(y_test, y_predict))[:5] + ' ' + str(precision_score(y_test, y_predict))[:5] + ' ' + str(recall_score(y_test, y_predict))[:5] + ' ' + str(f1_score(y_test, y_predict))[:5]
     
 
 
