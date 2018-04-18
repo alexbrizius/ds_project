f8b7e101f5dd54d5cb8de0866f64d138bc8cf17e
Alex Brizius <abrizius@nd.edu>
2018-04-18 12:12:35 -0400

    Added run_all.py script for simplicity

---
+++ run_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+import os
+import pydot
+import sys
+
+def usage(program):
+    print 'Usage: {} league'.format(program)
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) != 2:
+        usage(sys.argv[0])
+        sys.exit(1)
+
+    os.system("./parse.py " + sys.argv[1])
+    os.system("./overlap.py " + sys.argv[1])
+    os.system("./graphresults.py " + sys.argv[1])
