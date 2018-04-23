b19de79f8129afdf26d2d1a0357e75ef8de4f7d3
michael <momalle3@nd.edu>
2018-04-23 11:59:33 -0400

    mlset working correctly

--- mlset.py
+++ mlset.py
@@ -11,42 +11,58 @@ import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
 
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.svm import SVC 
-
+# Usage message
 def usage(program):
     print 'Usage: {} lowerLeague higherLeague'.format(program)
 
 
+### Main Execution ###
+
 if __name__ == '__main__':
     
+    # Usage check
     if len(sys.argv) != 3:
         usage(sys.argv[0])
         sys.exit(1)
 
-    l1_table_ordinal = pd.read_csv('./data/' + sys.argv[1] + '_overlap_ordinal.csv')
-    l1_table_numeric = pd.read_csv('./data/' + sys.argv[1] + '_overlap_numeric.csv')
+    # Read in tables
+    l1_ordinal = pd.read_csv('./data/' + sys.argv[1] + '_overlap_ordinal.csv')
+    l1_numeric = pd.read_csv('./data/' + sys.argv[1] + '_overlap_numeric.csv')
 
-    l2_table_ordinal = pd.read_csv('./data/' + sys.argv[2] + '_overlap_ordinal.csv')
-    l2_table_numeric = pd.read_csv('./data/' + sys.argv[2] + '_overlap_numeric.csv')
+    l2_ordinal = pd.read_csv('./data/' + sys.argv[2] + '_overlap_ordinal.csv')
+    l2_numeric = pd.read_csv('./data/' + sys.argv[2] + '_overlap_numeric.csv')
 
     # Find players that appear in both
-    numeric_overlap = pd.merge(l1_table_numeric, l2_table_numeric, how='inner', on=['Name'])
-    oridinal_overlap = pd.merge(l1_table_ordinal, l2_table_ordinal, how='inner', on=['Name'])
+    numeric_overlap = pd.merge(l1_numeric, l2_numeric, how='inner', on=['Name'])
+    ordinal_overlap = pd.merge(l1_ordinal, l2_ordinal, how='inner', on=['Name'])
+   
+    # Find players that appear only in the lower league
+    ll_numeric = pd.DataFrame(columns=['Name','PA',
+					'BB%','K%',
+					'AVG','OBP',
+					'SLG','OPS',
+					'BABIP','wRC+'])
+    num_names = list(numeric_overlap['Name'])
+    for index, row in l1_numeric.iterrows():
+	if row['Name'] in num_names:
+	    continue
+	else:
+	    ll_numeric.loc[len(ll_numeric)] = row
 
-    # Find player that appear only in the lower league
-    ll_numeric = l1_table_numeric[~l1_table_numeric['Name'].isin(l2_table_numeric)].dropna()
-    ll_ordinal = l1_table_ordinal[~l1_table_ordinal['Name'].isin(l2_table_ordinal)].dropna()
+    ll_ordinal = pd.DataFrame(columns=['Name','PA',
+					'BB%','K%',
+					'AVG','OBP',
+					'SLG','OPS',
+					'BABIP','wRC+'])
+    ord_names = list(ordinal_overlap['Name'])
+    for index, row in l1_ordinal.iterrows():
+	if row['Name'] in ord_names:
+	    continue
+	else:
+	    ll_ordinal.loc[len(ll_ordinal)] = row
 
+    print numeric_overlap
     print ll_numeric
+
 	
     
